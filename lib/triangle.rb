9a50905980bb1f1548151c1e8bf3c06b725850b9
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-11 04:12:43 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+attr_reader :side1, :side2, :side3, :tri_array
+  def initialize(side1,side2,side3)
+    @tri_array = []
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3 
+    @tri_array << side1
+    @tri_array << side2
+    @tri_array << side3
+ end
+
+   def kind
+  if @tri_array.any? { |side| side <= 0 }
+  raise TriangleError
+   elsif @tri_array[0] >= @tri_array[1] + @tri_array[2]
+   raise TriangleError
+   elsif @tri_array[1] >= @tri_array[0] + @tri_array[2]
+  raise TriangleError
+   elsif @tri_array[2] >= @tri_array[1] + @tri_array[0]
+    raise TriangleError
+   elsif @tri_array.uniq.size == 1
+    :equilateral
+    elsif @tri_array.uniq.size == 2
+    :isosceles
+    else
+    :scalene
+     end
+   end
+   class TriangleError < StandardError
+    # triangle error code
+  end
 end
