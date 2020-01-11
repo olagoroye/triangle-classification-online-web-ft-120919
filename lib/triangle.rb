d6d3bfe166f2096e560ceafd7e5dd6bc51edce93
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-11 03:26:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,6 @@
 class Triangle
  def initialize((side_1, side_2, side_3)
     @triangle_sides = []
-    @triangle_sides << side_1
-    @triangle_sides << side_2
-    @triangle_sides << side_3
+   
   end
 end
