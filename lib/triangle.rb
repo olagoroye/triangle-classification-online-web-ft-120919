bc449811b36899b1d3cf4100a2b260181ea5ac1f
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-11 04:07:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,18 +12,18 @@ attr_reader :side1, :side2, :side3, :tri_array
 
    def kind
   if @tri_array.any? { |side| side <= 0 }
-raise TriangleError
-elsif @tri_array[0] >= @tri_array[1] + @tri_array[2]
-raise TriangleError
-elsif @tri_array[1] >= @tri_array[0] + @tri_array[2]
-raise TriangleError
-elsif @tri_array[2] >= @tri_array[1] + @tri_array[0]
-raise TriangleError
-elsif @tri_array.uniq.size == 1
-:equilateral
-elsif @tri_array.uniq.size == 2
-:isosceles
-else
-:scalene
-end
+  raise TriangleError
+   elsif @tri_array[0] >= @tri_array[1] + @tri_array[2]
+   raise TriangleError
+   elsif @tri_array[1] >= @tri_array[0] + @tri_array[2]
+  raise TriangleError
+   elsif @tri_array[2] >= @tri_array[1] + @tri_array[0]
+    raise TriangleError
+   elsif @tri_array.uniq.size == 1
+    :equilateral
+    elsif @tri_array.uniq.size == 2
+    :isosceles
+    else
+    :scalene
+     end
 end
