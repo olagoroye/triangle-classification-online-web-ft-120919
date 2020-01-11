04293ac54454a425ed1bcdf0a6f257063b33b083
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-11 03:57:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,10 @@
 class Triangle
 attr_reader :side1, :side2, :side3, :tri_array
-def initialize(side1,side2,side3)
-@tri_array = []
-@side1 = side1
-@side2 = side2
-@side3 = side3
+  def initialize(side1,side2,side3)
+    @tri_array = []
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
 @tri_array << side1
 @tri_array << side2
 @tri_array << side3
